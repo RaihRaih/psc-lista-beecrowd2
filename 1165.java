ad9d24b5ada56376e30a62c8a2bbdc30e16b815c
Raissa Rodrigues <105363135+RaihRaih@users.noreply.github.com>
2024-04-25 18:30:08 -0300

    Update 1165.java

--- 1165.java
+++ 1165.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
-public class 1165 {
+public class Main {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int x = 0;
+    int x = 0, count = 0;
     System.out.println("Digite o numero de testes: ");
     int n = scanner.nextInt();
 
     for (int i = 0; i < n; i++) {
+      count = 0;
       x = scanner.nextInt();
+      for (int j = 2; j < x; j++) {
+        if (x % j == 0) {
+          count++;
+        }
+      }
 
-      if (x % 2 == 1 && x / x == 1) {
-        System.out.println(x + " eh primo\n");
-      } else if (x == 2) {
-        System.out.println(x + " eh primo\n");
+      if (count == 0) {
+        System.out.println(x + " eh primo");
       } else {
-        System.out.println(x + " nao eh primo\n");
+        System.out.println(x + " nao eh primo");
       }
     }
     scanner.close();
